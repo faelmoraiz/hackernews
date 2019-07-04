9e22ab21db78bd95d6bf091e3f0240c36185ddc1
faelmoraiz <faelmoraiz@gmail.com>
2019-07-04 17:10:25 -0300

    update.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const list = [
+  {
+    title: "React",
+    url: "https://facebook.github.io/react/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: "Redux",
+    url: "https://github.com/reactjs/redux",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  }
+];
+
+class App extends React.Component {
+  render() {
+    const dados = {
+      nome: "Rafael",
+      sobrenome: "Morais"
+    };
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <code>
+              Welcome {dados.nome} {dados.sobrenome}
+            </code>
+          </p>
+          {list.map(function(item) {
+            return (
+              <div key={item.objectID}>
+                <span> {item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+                <span>
+                  <a href={item.url}> {item.title}</a>
+                </span>
+              </div>
+            );
+          })}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// if (module.hot) {
+//   module.hot.accept();
+// }
